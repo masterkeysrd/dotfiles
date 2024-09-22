e0ce3090e3360b7c1f3da355ad3173c545a2eee9
David Morales <morales.david1997@gmail.com>
2024-09-21 22:39:09 -0400

    chore: add basic installation for mac

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Install script for the dotfiles
+
+# Install the dotfiles in a mac environment
+install_mac() {
+  if [ ! -d "$HOME/.dotfiles" ]; then
+    echo "Cloning dotfiles..."
+    git clone $DOTFILES_REPO $HOME/.dotfiles
+  else
+    echo "Updating dotfiles..."
+    git -C $HOME/.dotfiles pull
+  fi
+
+  echo "Installing dotfiles..."
+  $HOME/.dotfiles/install_mac.sh
+}
+
+install() {
+  if [ "$(uname)" == "Darwin" ]; then
+    install_mac
+  else
+    echo "Unsupported OS"
+  fi
+}
---
+++ install_mac.sh
@@ -0,0 +1,21 @@
+!/usr/bin/env bash
+# Install script for the dotfiles on a mac environment
+
+install_brew() {
+  if ! command -v brew &>/dev/null; then
+    echo "Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  else
+    echo "Homebrew already installed"
+  fi
+}
+
+install_brew_packages() {
+  echo "Installing brew packages..."
+  brew bundle --file=$HOME/.dotfiles/mac/Brewfile
+}
+
+
+# Install the dotfiles in a mac environment
+install_brew()
+install_brew_packages()
