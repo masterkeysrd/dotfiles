510c39cac333b03da16d1df9de3d0f3543b8360d
David Morales <morales.david1997@gmail.com>
2024-12-09 08:49:03 -0400

    chore: update to create load_localrc as function

--- .config/masterkeys/zsh_init.sh
+++ .config/masterkeys/zsh_init.sh
@@ -11,9 +11,13 @@ if [ -z "$TMUX" ]; then
   start_tmux
 fi
 
-# Check if .localrc exists and source it
-# This file is used to store local environment variables
-# It is not tracked by git
-if [ -f ~/.localrc ]; then
-  source ~/.localrc
-fi
+load_localrc() {
+  # Check if .localrc exists and source it
+  # This file is used to store local environment variables
+  # It is not tracked by git
+  if [ -f ~/.localrc ]; then
+    source ~/.localrc
+  fi
+}
+
+load_localrc
