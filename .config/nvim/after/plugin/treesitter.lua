33e159b0b6271d487e9a906ecc9bc10cf5e4d625
David Morales <morales.david1997@gmail.com>
2025-06-23 00:15:51 -0400

    chore(nvim): install and setup treesitter

---
+++ .config/nvim/after/plugin/treesitter.lua
@@ -0,0 +1,28 @@
+require('nvim-treesitter.configs').setup({
+    modules = {},
+    ensure_installed = { 'lua', 'go' },
+    ignore_install = {},
+    auto_install = true,
+    sync_install = false,
+    highlight = {
+        enable = true,
+        disable = function(_, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
+        additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+        },
+    },
+
+})
