b8d7ebe867042eda01ed4e557adacfc8199d5130
David Morales <morales.david1997@gmail.com>
2025-06-25 02:08:43 -0400

    feat(nvim): add gitsigns support

---
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -0,0 +1,38 @@
+local gs = require('gitsigns')
+local icons = require('masterkeysrd.icons')
+
+gs.setup({
+    signs = {
+        add = { text = icons.misc.vertical_bar },
+        change = { text = icons.misc.vertical_bar },
+        delete = { text = icons.misc.right_arrow },
+        topdelete = { text = icons.misc.right_arrow },
+        changedelete = { text = icons.misc.vertical_bar },
+        untracked = { text = icons.misc.vertical_bar },
+    },
+    signs_staged = {
+        add = { text = icons.misc.vertical_bar },
+        change = { text = icons.misc.vertical_bar },
+        delete = { text = icons.misc.right_arrow },
+        topdelete = { text = icons.misc.right_arrow },
+        changedelete = { text = icons.misc.vertical_bar },
+        untracked = { text = icons.misc.vertical_bar },
+    },
+    on_attach = function(bufnr)
+        vim.api.nvim_buf_set_var(bufnr, 'gitsigns_status_dict', {})
+        local function opts(desc)
+            return { desc = desc, buffer = bufnr }
+        end
+
+        local map = vim.keymap.set
+
+        map('n', '[g', gs.prev_hunk, opts('Previous hunk'))
+        map('n', ']g', gs.next_hunk, opts('Next hunk'))
+
+        map('n', '<leader>gb', gs.toggle_current_line_blame, opts "Toggle blame")
+        map('n', '<leader>gp', gs.preview_hunk, opts "Preview hunk")
+        map('n', '<leader>gr', gs.reset_hunk, opts "Reset hunk")
+        map('n', '<leader>gR', gs.reset_buffer, opts "Reset buffer")
+        map('n', '<leader>gs', gs.stage_hunk, opts "Reset buffer")
+    end,
+})
