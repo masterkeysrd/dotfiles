aba0ceb71b500a8740145d07b2e9a09124973ece
David Morales <morales.david1997@gmail.com>
2025-06-26 23:54:53 -0400

    feat(nvim): add autocmds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,5 @@
-require("masterkeysrd")
+-- Set my colorscheme
 vim.cmd.colorscheme("wonderland")
+
+-- General setup
+require("masterkeysrd")
--- .config/nvim/lua/masterkeysrd/init.lua
+++ .config/nvim/lua/masterkeysrd/init.lua
@@ -1,2 +1,3 @@
 require("masterkeysrd.keymap")
 require("masterkeysrd.set")
+require("masterkeysrd.autocmds")
