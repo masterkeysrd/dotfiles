7f6d145d4c3e8dbfbbc18b3fb2377781e4909507
David Morales <morales.david1997@gmail.com>
2025-06-29 22:58:40 -0400

    feat(tmux): add colorscheme integration

--- .config/masterkeys/zsh_custom.sh
+++ .config/masterkeys/zsh_custom.sh
@@ -3,25 +3,28 @@
 #
 # This file is sourced in the .zshrc file
 
-base_moon=234
-rose_moon=217
-pine_moon=74
-gold_moon=220
-iris_moon=135
-surface_moon=235
-
 branch_icon="\ue0a0"
 detached_icon="\u27a6"
 plusminus_icon="\u00b1"
 
+
+canvas_subtle=234
+success_fg=69
+
+scale_green_03=77
+scale_red_04=210
+scale_purple_03=183
+scale_orange_03=215
+
+
 prompt_context() {
   if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
-    prompt_segment $surface_moon default "👻"
+    prompt_segment $canvas_subtle default "👻"
   fi
 }
 
 prompt_dir() {
-  prompt_segment $rose_moon $base_moon "%3~"
+  prompt_segment $scale_green_03 $canvas_subtle "%3~"
 }
 
 prompt_git() {
@@ -32,12 +35,12 @@ prompt_git() {
 
   ref=$(git symbolic-ref --short HEAD 2>/dev/null) || ref=$(git rev-parse --short HEAD 2>/dev/null) || ref=$(git rev-parse --short --verify HEAD 2>/dev/null)
   if [[ -n $ref ]]; then
-    bg_color=$surface_moon
+    bg_color=$canvas_subtle
     if is_dirty; then
-      fg_color=$rose_moon
+      fg_color=$success_fg
       ref="$ref $plusminus_icon"
     else
-      fg_color=$pine_moon
+      fg_color=$scale_green_03
       ref="$ref"
     fi
 
@@ -57,19 +60,19 @@ prompt_git() {
 typeset -A ZSH_HIGHLIGHT_STYLES
 
 # To change the style of the command based on the return code
-ZSH_HIGHLIGHT_STYLES[builtin]="fg=${pine_moon}"
+ZSH_HIGHLIGHT_STYLES[builtin]="fg=${scale_red_04}"
 
 # To differentiate aliases from other command types
-ZSH_HIGHLIGHT_STYLES[alias]="fg=${rose_moon}"
+ZSH_HIGHLIGHT_STYLES[alias]="fg=${scale_purple_03}"
 
 # To have the command in a different color
-ZSH_HIGHLIGHT_STYLES[command]="fg=${iris_moon}"
+ZSH_HIGHLIGHT_STYLES[command]="fg=${scale_purple_03}"
 
 # To have reserved words in a different color
-ZSH_HIGHLIGHT_STYLES['reserved-word']="fg=${gold_moon}"
+ZSH_HIGHLIGHT_STYLES['reserved-word']="fg=${scale_red_04}"
 
 # To further differentiate environment variables
-ZSH_HIGHLIGHT_STYLES[function]="fg=${rose_moon}"
+ZSH_HIGHLIGHT_STYLES[function]="fg=${scale_purple_03}"
 
 # To have paths colored instead of underlined
 ZSH_HIGHLIGHT_STYLES[path]='fg=white,underline'
