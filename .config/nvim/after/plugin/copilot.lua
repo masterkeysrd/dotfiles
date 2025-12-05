5eb75880e44e0e77c20ec3e4c5148fc5e1de231a
David Morales <morales.david1997@gmail.com>
2025-12-05 13:24:55 -0400

    feat(nvim): add copilot keymaps

--- .config/nvim/after/plugin/copilot.lua
+++ .config/nvim/after/plugin/copilot.lua
@@ -2,4 +2,5 @@ vim.g.copilot_no_tab_map = false
 
 vim.keymap.set('i', '<C-j>', '<Plug>(copilot-next)', { noremap = false })
 vim.keymap.set('i', '<C-k>', '<Plug>(copilot-previous)', { noremap = false })
-vim.keymap.set('i', '<S-CR>', '<Plug>(copilot-accept-line)', { noremap = false })
+vim.keymap.set('i', '<S-Tab>', '<Plug>(copilot-accept-line)', { noremap = false })
+vim.keymap.set('i', '<C-y>', '<Plug>(copilot-accept)', { noremap = false })
