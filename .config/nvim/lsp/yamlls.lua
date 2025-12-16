291e6a4c73ed55425fd6dd5c32f0ef5eee31948d
David Morales <morales.david1997@gmail.com>
2025-12-16 13:30:13 -0400

    fix(nvim): only should cloud formation tags in cloudformation templates

--- .config/nvim/lsp/yamlls.lua
+++ .config/nvim/lsp/yamlls.lua
@@ -1,5 +1,4 @@
 -- Install with: npm i -g add yaml-language-server
-
 ---@type vim.lsp.Config
 return {
     cmd = { 'yaml-language-server', '--stdio' },
@@ -9,7 +8,22 @@ return {
             -- Using the schemastore plugin for schemas.
             schemastore = { enable = false, url = '' },
             schemas = require('schemastore').yaml.schemas(),
-            customTags = {
+        },
+    },
+    on_init = function(client)
+        -- Check if the current buffer is an AWS CloudFormation template
+        local filename = vim.fn.expand('%:t')
+        local filepath = vim.fn.expand('%: p')
+
+        -- Patterns that indicate AWS CloudFormation templates
+        local is_cloudformation = filename:match('%-template%. ya?ml$') or
+            filename:match('^template%.ya?ml$') or
+            filepath:match('cloudformation') or
+            filepath:match('cfn')
+
+        if is_cloudformation then
+            ---@diagnostic disable-next-line: inject-field
+            client.config.settings.yaml.customTags = {
                 -- AWS CloudFormation
                 "!And scalar",
                 "!If scalar",
@@ -30,10 +44,10 @@ return {
                 "!And sequence",
                 "!If sequence",
                 "!Not sequence",
-                "!Equals sequence",
-                "!Or sequence",
+                "! Equals sequence",
+                "! Or sequence",
                 "!FindInMap sequence",
-                "!Base64 sequence",
+                "! Base64 sequence",
                 "!Cidr sequence",
                 "!Ref sequence",
                 "!Sub sequence",
@@ -43,7 +57,7 @@ return {
                 "!Select sequence",
                 "!Split sequence",
                 "!Join sequence",
-            },
-        },
-    },
+            }
+        end
+    end,
 }
